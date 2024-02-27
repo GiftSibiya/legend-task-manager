153ba0653f77e1291bd678ca1740f2f56a544d10
GiftSibiya <giftsibiya.gb@gmail.com>
2024-02-27 18:57:16 +0200

    Basic Backend Connection

--- backend/Server.js
+++ backend/Server.js
@@ -1,9 +1,25 @@
 /// Importing dependencies ///
 
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DBURL =
+  "mongodb+srv://sibiyabobo:b0b0@cluster0.xe42gpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 /// ROUTES ///
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(DBURL);
+    console.log("Connected to mongo");
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -13,6 +29,8 @@ app.post("/submit", (req, res) => {
   const data = req.body;
   res.json({ message: "Data received successfully", data });
 });
+
+connectToDb();
 
 // Start our server:
 app.listen(3001, () => {
