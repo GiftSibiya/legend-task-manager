a4abe0d865a2449bdb98d79024ffd3b7d45999c3
GiftSibiya <giftsibiya.gb@gmail.com>
2024-02-27 19:45:00 +0200

    password creation

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -6,13 +6,19 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const password = req.body.password;
   let user = await User.findOne({ email: req.body.email });
+
+  if (!password || typeof password !== "string") {
+    return res.status(400).send("Invalid password");
+  }
+
   if (user) return res.send("This email is already in use");
 
   user = await new User({
     name: req.body.name,
     email: req.body.email,
-    password: await bcrypt.hash(req.body.password, 10),
+    password: await bcrypt.hash(password, 10),
   }).save();
 
   res.send(user);
